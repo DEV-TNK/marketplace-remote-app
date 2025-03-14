5d18829d7972f73a3c8fd3246f4d44c16515cc6c
ipsychDev <joy@gfa-tech.com>
2025-03-14 18:14:51 +0100

    fixed email verification error.

--- src/utils/sendEMail.js
+++ src/utils/sendEMail.js
@@ -1,20 +1,24 @@
 const nodemailer = require("nodemailer");
 const nodeMailerConfig = require("../config/nodeMailer");
-
 const sendEmail = async ({ html, to, subject }) => {
   const transporter = nodemailer.createTransport(nodeMailerConfig);
 
-  try {
-    const info = await transporter.sendMail({
-      from: `"MARKETPLACE" <info@cipme.ci>`,
-      html,
-      to,
-      subject,
-    });
-    return info;
-  } catch (error) {
-    throw error;
-  }
-};
+    try {
+        const info = await transporter.sendMail({
+            from: `"MARKETPLACE" <info@cipme.ci>`,
+            html,
+            to,
+            subject
+        });
+        return info;
+    } catch (error) {
+        throw error;
+    }
+}
+
 
 module.exports = sendEmail;
+
+
+
+
